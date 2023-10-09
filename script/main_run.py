ff0a66ac12ec9722dca96b8c5f959b2c947aa747
jaybfn <jayesh.bfn@gmail.com>
2023-10-09 13:32:18 +0200

    adding CI file

--- script/main_run.py
+++ script/main_run.py
@@ -12,15 +12,16 @@ msg = "updated new CSV file!"
 #os.environ['GIT_SSH_COMMAND'] =  'ssh -i /c/Users/Asus/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 repo_directory = os.getcwd()
 subprocess.run(['python', 'ETL.py'])
-# Set up Git credential helper
-subprocess.run(["git", "config", "--global", "credential.helper", "store"])
 
-# Add and commit your changes
-subprocess.run(["git", "add", "."], cwd=repo_directory)
-subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
+# # Set up Git credential helper
+# subprocess.run(["git", "config", "--global", "credential.helper", "store"])
 
-# Push without being prompted for credentials
-subprocess.run(["git", "push"], cwd=repo_directory)
+# # Add and commit your changes
+# subprocess.run(["git", "add", "."], cwd=repo_directory)
+# subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
+
+# # Push without being prompted for credentials
+# subprocess.run(["git", "push"], cwd=repo_directory)
 
 # run the app!
-# subprocess.run(['streamlit', 'run', 'app.py'])
+subprocess.run(['streamlit', 'run', 'app.py'])
