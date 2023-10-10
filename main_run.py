c7090914ebcd0ec77d86735c0f906e7697e30297
jaybfn <jayesh.bfn@gmail.com>
2023-10-10 15:59:36 +0200

    updates

--- main_run.py
+++ main_run.py
@@ -8,29 +8,12 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 # message for the git commit!
 msg = "updated new CSV file!"
 
-# Set the environment variable for SSH authentication
-#os.environ['GIT_SSH_COMMAND'] =  'ssh -i /c/Users/Asus/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 repo_directory = os.getcwd()
 subprocess.run(['python', 'ETL.py'])
 
-# # Set up Git credential helper
-subprocess.run(["git", "config", "--global", "credential.helper", "store"])
-
-# # Add and commit your changes
-subprocess.run(["git", "add", "."], cwd=repo_directory)
-subprocess.run(["git", "commit", "-m", msg], cwd=repo_directory)
-
-# # Push without being prompted for credentials
-subprocess.run(["git", "push"], cwd=repo_directory)
-
 # run the app!
 try:
-<<<<<<< HEAD:fx_analytics/main_run.py
     subprocess.run(['streamlit', 'run', 'app.py'])
 except KeyboardInterrupt:
     print("Streamlit app was stopped.")
-=======
-	subprocess.run(['streamlit', 'run', 'app.py'])
-except:
-	print("Streamlit app was stopped.")
->>>>>>> bd7ca6e313a48d4b47b1e89f46dee59756dc1d51:main_run.py
+
