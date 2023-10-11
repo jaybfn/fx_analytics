d9157b28ba1667b02df2e257eb8294c3d84d7c77
jaybfn <jayesh.bfn@gmail.com>
2023-10-11 09:52:17 +0200

    updates

--- fx_analytics/credential.py
+++ fx_analytics/credential.py
@@ -1,1 +1,2 @@
+
 mt5_credentials = {'login': '*****', 'server':'*****','password':'*****'}
--- fx_analytics/main_run.py
+++ fx_analytics/main_run.py
@@ -1,16 +1,41 @@
 
 import subprocess
 import os
+from typing import NoReturn
 
-# Set the current working directory to the folder containing your scripts
-script_directory = os.path.dirname(os.path.abspath(__file__))
+def run_scripts(script_path: str) -> NoReturn:
+    """
+    Executes specific Python scripts located in a given directory.
 
-repo_directory = os.getcwd()
-subprocess.run(['python', 'ETL.py'])
+    This function first changes the current working directory to the one specified,
+    then executes an ETL.py script, and finally runs a Streamlit application.
+    If the Streamlit application is stopped by the user, a message is printed.
 
-# run the app!
-try:
-    subprocess.run(['streamlit', 'run', 'app.py'])
-except KeyboardInterrupt:
-    print("Streamlit app was stopped.")
+    Args:
+    script_path (str): The file path of the script directory.
+
+    Raises:
+    KeyboardInterrupt: If the Streamlit application is stopped by the user.
+
+    Returns:
+    NoReturn: This function does not return anything, it's meant for script execution only.
+    """
+
+    # Set the current working directory to the folder containing your scripts
+    os.chdir(script_path)
+
+    # Execute the ETL script
+    subprocess.run(['python', 'ETL.py'])
+
+    # Run the Streamlit app
+    try:
+        subprocess.run(['streamlit', 'run', 'app.py'])
+    except KeyboardInterrupt:
+        print("Streamlit app was stopped.")
+
+
+if __name__ == "__main__":
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    run_scripts(script_directory)
+
 
