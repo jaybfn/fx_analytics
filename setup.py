4bee08664b0e670b6b34a1171db28f3dd03177fb
jaybfn <jayesh.bfn@gmail.com>
2023-10-15 13:08:16 +0200

    build_deploy

--- setup.py
+++ setup.py
@@ -3,20 +3,26 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+
 setup(
     name="fx_analytics",
-    version="1.5.2",
+    version="1.5.3",
     description = "A streamlit dashboard which displays all the forex metric from your trading platforms",
     packages=['fx_analytics'],
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url= "https://github.com/jaybfn/forex_dashboard",
+    url= "https://github.com/jaybfn/fx_analytics",
     author = "Jayesh Arun Bafna",
     author_email = "jayesh.bfn@gmail.com",
     license= "MIT",
     extras_require = {"dev":["pytest","twine"],},
-    install_requires=required,
+    install_requires=['pandas>=2.1.0',
+                        'streamlit>=1.26.0',
+                        'plotly>=5.16.1',
+                        'plotly-express>=0.4.1',
+                        'loguru>=0.7.2',
+                        'wheel',
+                        'pytest'
+                        ],
     python_requires=">=3.11"
 )
