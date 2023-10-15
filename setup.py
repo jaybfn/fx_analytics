2ff2918fb952467545df5dc06a35a6ee2ae738c8
jaybfn <jayesh.bfn@gmail.com>
2023-10-15 12:35:59 +0200

    build_deploy

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 setup(
     name="fx_analytics",
-    version="1.5.1",
+    version="1.5.2",
     description = "A streamlit dashboard which displays all the forex metric from your trading platforms",
     packages=['fx_analytics'],
     long_description = long_description,
